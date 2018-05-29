b36c53e24bd017984c5f9d4c30e5edd8c9677269
totalfreak <dgkartin@live.dk>
2018-05-29 22:20:23 +0200

    Finished DAE7, and added many later usable functions

--- DAE7/DAE7.r
+++ DAE7/DAE7.r
@@ -5,10 +5,7 @@ library(nortest)
 library(R.matlab)
 library(reshape2)
 library(ggplot2)
-
-se <- function(x) {
-    sqrt(sd(x)/length(x))
-}
+library(ggpubr)
 
 sem <- function(x){
   sd(x)/sqrt(length(x))
@@ -18,17 +15,14 @@ cohens_d <- function(x, y) {
     lenX <- length(x) - 1
     lenY <- length(y) - 1
     meanDiff <- abs(mean(x) - mean(y))
-    csd <- lenX * var(x) + lenY * var(y)
-    csd <- csd/(lenX + lenY)
-    csd <- sqrt(csd)
+    pssd <- lenX * var(x) + lenY * var(y)
+    pssd <- pssd/(lenX + lenY)
+    pssd <- sqrt(pssd)
     print("Cohen's D")
-    print(cd <- meanDiff/csd)
+    print(cd <- meanDiff/pssd)
 }
 
 # 1
-lambda <- 2
-mu <- 1/lambda
-N <- 40
 
 # a
 chinese <- readMat('ChineseEnglish.mat')
@@ -36,10 +30,10 @@ data <- data.frame(ch=chinese$x.ch, us=chinese$x.us)
 
 print('Chinese mean and standard error')
 mean(data$ch)
-se(data$ch)
+sem(data$ch)
 print('American mean and standard error')
 mean(data$us)
-se(data$us)
+sem(data$us)
 png('DAE7-1a.png')
 hist(data$ch, xlim=c(4, 16), ylim=c(0, 14), xlab='data$ch & data$us',  main = paste("Histogram of", 'data$ch and data$us'), col = 'skyblue')
 hist(data$us, add=T, col = scales::alpha('red', .4))
@@ -48,26 +42,24 @@ dev.off()
 # b
 # Interval level of measurement
 png('DAE7-1b1.png')
-qqnorm(data$ch, col='red', pch=3)
-qqline(data$ch)
+ggqqplot(data$ch)
 dev.off()
 shapiro.test(data$ch)
 
 png('DAE7-1b2.png')
-qqnorm(data$us, col='red', pch=3)
-qqline(data$us)
+ggqqplot(data$us)
 dev.off()
 shapiro.test(data$us)
 
 # c
-# H0 = Chinese speaking are equally good singers as american
-# H1 = Chinese speaking are better singers than american
+# H0 = Chinese speaking are as good singers on average as american speaking
+# H1 = Chinese speaking are better on average than american speaking
 # Two sample
 # Independent I guess
 # Dunno about no tails
-# Yes, H0 rejected, mean is bigger.
+# Yes, H0 rejected, mean of chinese is bigger.
 
-ttest <- t.test(data$ch, data$us, alternative='greater', conf.level=0.99, mu=10.49732)
+ttest <- t.test(data$ch, data$us, conf.level=0.99)
 ttest
 
 # d
@@ -94,6 +86,9 @@ after.mean <- mean(weights$after)
 before.sd <- sd(weights$before)
 after.sd <- sd(weights$after)
 
+c(before.mean, before.sd)
+c(after.mean, after.sd)
+
 png('DAE7-2a.png')
 hist(weights$before, xlim=c(60, 120), ylim=c(0, 10), xlab='weights$before & weights$after',  main = paste("Histogram of", 'weights$before and weights$after'), col = 'skyblue')
 hist(weights$after, add=T, col = scales::alpha('red', .4))
@@ -103,13 +98,11 @@ dev.off()
 # b
 # Interval level of measurement I guess
 png('DAE7-2b1.png')
-qqnorm(weights$before, col='red', pch=3)
-qqline(weights$before)
+ggqqplot(weights$before)
 dev.off()
 
 png('DAE7-2b2.png')
-qqnorm(weights$after, col='red', pch=3)
-qqline(weights$after)
+ggqqplot(weights$after)
 dev.off()
 # Both look like they come from a normal distribution
 shapiro.test(weights$before) # w = 0.95608 p = 0.3419
@@ -121,7 +114,7 @@ after.mean # 93.82223
 # c
 # H0 = The weights will not have changed
 # H1 = The weights will have decreased
-t.test(weights$before, weights$after, pair=T, alternative='less', conf.level=.95, mu=92.4223)
+t.test(weights$before, weights$after, pair=T, alternative='less', conf.level=.9995)
 # Do paired test to see if mean difference proofs weight loss.
 # Dependent as the samples are from the same people.
 # Reject null hypothesis based on t test, as t = -2.1433 and p-value = 0.02122, but can't accept alternative hypothesis either.
@@ -143,7 +136,60 @@ heights <- data.frame(du=heights$x.du, da=heights$x.da)
 du.mean <- mean(heights$du)
 da.mean <- mean(heights$da)
 
+du.se <- sem(heights$du)
+da.se <- sem(heights$da)
+
+c(du.mean, du.se)
+c(da.mean, da.se)
+
+png('DAE7-3a.png')
+hist(heights$du, xlab='heights$du & heights$da',  main = paste("Histogram of", 'heights$du and heights$da'), col = 'skyblue')
+hist(heights$da, add=T, col = scales::alpha('red', .4))
+legend("topright", c("heights$du", "heights$da"), fill=c(scales::alpha('skyblue', .8), scales::alpha('red', .8)))
+dev.off()
 # b
+# Ratio level of measurement I guess
+png('DAE7-3b1.png')
+ggqqplot(heights$du)
+dev.off()
+
+png('DAE7-3b2.png')
+ggqqplot(heights$da)
+dev.off()
+
+shapiro.test(heights$du) # W = 0.97796 p-value = 0.8418
+shapiro.test(heights$da) # W = 0.97478 p-value = 0.7664
+# Test shows that both are from a normal distribution"with p-values of 0.8418 & 0.7664
+# Both look to be drawn from a normal distribution.
+
+# c
+# H0 = Danish and Dutch males aged 20-25 dont' differ in height on average
+# H1 = Danish and Dutch males aged 20-25 differ in height on average
+
+# Doing an F test shows that the two samples don't have the same variance, with a F value = 1.356
+var.test(heights$du, heights$da, conf.level=.95)
+
+t.test(heights$du, heights$da)
+# t = 2.3117 p-value = 0.02523
+# H0 rejected
+
+# d
+theD <- cohens_d(heights$du, heights$da)
+# theD = 0.6538611
+# Close to medium effect size
+
+# f
+sme <- apply(heights, 2, mean)
+ssem <- apply(heights, 2, sem)
+sme_ssem <- data.frame(mean = sme, sem = ssem, group=names(heights))
+
+png('DAE7-3f.png')
+ggplot(sme_ssem, aes(x = group, y = mean)) + geom_bar(stat = 'identity') + geom_errorbar(aes(ymin = mean - sem, ymax = mean + sem), width = .2, col='red') + xlab('Means') + ylab('Value')
+dev.off()
+
+
+
+
 
 
 
